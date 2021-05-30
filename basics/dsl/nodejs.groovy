80a5c248449c111bbbb834daa49511855f422fd1
eranp-dev <84897021+eranp-dev@users.noreply.github.com>
2021-05-30 11:37:19 +0300

    Update nodejs.groovy

--- basics/dsl/nodejs.groovy
+++ basics/dsl/nodejs.groovy
@@ -1,4 +1,4 @@
-ob('NodeJS Docker example') {
+job('NodeJS Docker example') {
     scm {
         git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
